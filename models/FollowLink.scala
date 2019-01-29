8f45fa196c130ea9e5b0bcae9d83313edf844877
Ahmad Owais <ahmadowais2@gmail.com>
2019-01-28 22:37:56 -0500

    Add files via upload

---
+++ models/FollowLink.scala
@@ -0,0 +1,11 @@
+/**
+ * Copyright mohaahma. Licensed under the Apache License, Version 2.0
+ * See accompanying LICENSE file.
+ */
+
+package models
+
+/**
+ * @author mohaahma
+ */
+case class FollowLink(title: String, url: String)
---
+++ models/form/PreferredReplicaElectionOperation.scala
@@ -0,0 +1,31 @@
+/**
+ * Copyright mohaahma. Licensed under the Apache License, Version 2.0
+ * See accompanying LICENSE file.
+ */
+
+package models.form
+
+/**
+ * @author mohaahma
+ */
+sealed trait PreferredReplicaElectionOperation
+
+case object RunElection extends PreferredReplicaElectionOperation
+case class UnknownPREO(op: String) extends PreferredReplicaElectionOperation
+
+object PreferredReplicaElectionOperation {
+  def apply(s: String) : PreferredReplicaElectionOperation = {
+    s match {
+      case "run" => RunElection
+      case a => UnknownPREO(a)
+    }
+  }
+
+  def unapply(op: PreferredReplicaElectionOperation) : Option[String] = {
+    op match {
+      case RunElection => Some("run")
+      case UnknownPREO(_) => None
+    }
+  }
+
+}
